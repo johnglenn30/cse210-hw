d611a4188b59b8793a8d4ae8ec63c5cd11773c25
John Glenn Elisan <jglenn8930@gmail.com>
2024-03-05 13:01:42 +0800

    Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,46 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+
+        DisplayMessage();
+
+        string userName = YourUserName();
+        int userNumber = YourNumber();
+
+        int squaredNumber = NumberSquared(userNumber);
+
+        ShowResult(userName, squaredNumber);
+    }
+
+    static void DisplayMessage() 
+    {
+        Console.WriteLine("Welcome!");
+    }
+
+    static string YourUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+    static int YourNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int NumberSquared(int number)
+    {
+        int square = number * number;
+        return square;
+
+    }
+
+    static void ShowResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+
+        job1._jobTitle = "Software Engineer";
+
+        Console.WriteLine(job1._jobTitle);
+
+
+        
     }
 }
