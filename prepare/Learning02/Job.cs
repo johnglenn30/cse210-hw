4c1531471f5887b75c1c412e29bef160c438eb2e
John Glenn Elisan <jglenn8930@gmail.com>
2024-03-06 00:31:14 +0800

    Learning02

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -1,5 +1,15 @@
+
 public class Job
 {
     public string _jobTitle;
-    
+    public string _companyNow;
+    public int _startYear;
+    public int _endYear;
+
+    public void DisplayTitle()
+    {
+        Console.WriteLine($"{_jobTitle},{_companyNow},{_startYear}-{_endYear}");
+    }
+
+   
 }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,15 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Console.WriteLine("Hello Learning02 World!");
 
         Job job1 = new Job();
 
         job1._jobTitle = "Software Engineer";
+        job1._companyNow = "(Microsoft)";
+        job1._startYear = 2011;
+        job1._endYear = 2014;
+        job1.DisplayTitle();
 
-        Console.WriteLine(job1._jobTitle);
+        Job job2 = new Job();
+
+        job2._jobTitle = "Manager";
+        job2._companyNow = "Apple";
+        job2._startYear = 2015;
+        job2._endYear = 2023;
+        job2.DisplayTitle();
+
+        Resume newResume = new Resume();
+        newResume._name = "John";
+
+        newResume._jobs.Add(job1);
+        newResume._jobs.Add(job2);
+
+        newResume.DisplayResume();
 
 
+        
         
     }
 }
